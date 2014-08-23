2696bd1152e926039873d2d62cb979bd7994d9b0
Yichao Yu <yyc1992@gmail.com>
2014-08-23 17:04:44 -0400

    [test] test-override-mount

---
+++ test/test-override-mount.cpp
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *   Copyright (C) 2014~2014 by Yichao Yu                                  *
+ *   yyc1992@gmail.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
+#include <lns_tools/program.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <sched.h>
+#include <unistd.h>
+
+static void
+make_dir(const char *name)
+{
+    struct stat stat_buf;
+    if (lstat(name, &stat_buf) == 0) {
+        if (!S_ISDIR(stat_buf.st_mode)) {
+            fprintf(stderr,
+                    "path '%s' already exist and is not a directory.", name);
+            exit(-1);
+        }
+    } else if (mkdir(name, 0750) == -1) {
+        assert_perror(errno);
+    }
+}
+
+int
+main2()
+{
+    LNSTools::Program p("tree");
+    p.set_args("tree", "-C", "/tmp/tmp2");
+
+    int pid = p.run();
+    assert_perror(pid <= 0 ? errno : 0);
+    int status = 0;
+    p.wait(&status);
+    assert_perror(status);
+    return 0;
+}
+
+int
+main()
+{
+    LNSTools::Process p(main2);
+    p.set_userns(true);
+    p.add_uid_map(0, getuid());
+    p.add_gid_map(0, getgid());
+
+    p.chroot("/");
+    make_dir("/tmp/tmp2");
+    p.add_mount_map(".", "/tmp/tmp2");
+
+    int pid = p.run();
+    assert_perror(pid <= 0 ? errno : 0);
+    int status = 0;
+    p.wait(&status);
+    assert_perror(status);
+    return 0;
+}
